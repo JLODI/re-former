1d3dccd85af5eddad6eb4ac559789d47acb6c8d5
JLODI <88990920+JLODI@users.noreply.github.com>
2024-12-18 01:58:10 -0800

    added edit route and methods on user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,19 @@ class UsersController < ApplicationController
         # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
         @user = User.new(user_params)
             if @user.save
+                redirect_to 
+            else
+                render :new, status: :unprocessable_entity
+            end
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+            if User.update(user_params)
                 redirect_to new_user_path
             else
                 render :new, status: :unprocessable_entity
